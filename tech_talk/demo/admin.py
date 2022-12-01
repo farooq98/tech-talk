db5ded2d3926d8bf230350ac41fa4e2a5de5fd4d
Farooq Hidayat <farooqhd98@gmail.com>
2022-12-01 12:18:47 +0500

    django-admin-features

--- tech_talk/demo/admin.py
+++ tech_talk/demo/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Product, Order, OrderLine
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+class OrderLineAdmin(admin.StackedInline):
+    model = OrderLine
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderLineAdmin]
+    list_display = ["id", "customer_name", "total", "contact", "email", "status", "cod"]
+    list_editable = ["status", "cod"]
+    list_filter = ["status", "cod"]
+    date_hierarchy = "demo_order_date"
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderLine)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+# admin.site.register(OrderLine)
